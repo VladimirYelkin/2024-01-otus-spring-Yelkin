50d8ddb69ffb99ed287302fec6fe330395653199
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-10-04 14:59:35 +0400

    HW01: added DI in ConsoleIOService

--- HW01/src/main/java/ru/otus/service/ConsoleIOService.java
+++ HW01/src/main/java/ru/otus/service/ConsoleIOService.java
@@ -1,8 +1,17 @@
 package ru.otus.service;
 
+import java.io.PrintStream;
+
 public class ConsoleIOService implements IOService {
+
+    private final PrintStream printStream;
+
+    public ConsoleIOService(PrintStream printStream) {
+        this.printStream = printStream;
+    }
+
     @Override
     public void println(String string) {
-        System.out.println(string);
+        printStream.println(string);
     }
 }

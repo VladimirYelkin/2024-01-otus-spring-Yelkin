bf7eea286df455fc7aef47b306c4cfe8e68e6ffa
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-10-11 17:55:32 +0400

    HomeWork2 copied sources from HW1

---
+++ HW02/src/main/java/ru/otus/dao/CsvQuestionDao.java
@@ -0,0 +1,42 @@
+package ru.otus.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import ru.otus.config.TestFileNameProvider;
+import ru.otus.dao.dto.QuestionDto;
+import ru.otus.exeption.QuestionDaoException;
+import ru.otus.model.Question;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class CsvQuestionDao implements QuestionDao {
+
+    private final TestFileNameProvider fileName;
+
+    public CsvQuestionDao(TestFileNameProvider testFileNameProvider) {
+        this.fileName = testFileNameProvider;
+    }
+
+    @Override
+    public List<Question> findAll() {
+
+        try (InputStream inputStream = getClass().getClassLoader().
+                getResourceAsStream(fileName.getTestFileName());
+             var streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+            var csvToBean = new CsvToBeanBuilder<QuestionDto>(streamReader)
+                    .withType(QuestionDto.class)
+                    .withSeparator(';').build();
+            return csvToBean.stream()
+                    .map(QuestionDto::toDomainObject)
+                    .collect(Collectors.toList());
+        } catch (SecurityException | NullPointerException | IOException e) {
+            throw new QuestionDaoException(e);
+        }
+
+    }
+}
---
+++ HW02/src/test/java/ru/otus/dao/CsvQuestionDaoTest.java
@@ -0,0 +1,79 @@
+package ru.otus.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import ru.otus.config.TestFileNameProvider;
+import ru.otus.model.Answer;
+import ru.otus.model.Question;
+
+import java.util.List;
+
+@DisplayName("QuestDaoCsv")
+class CsvQuestionDaoTest {
+
+
+    private final List<Question> questionList = List.of(
+            new Question(1L, "question1",
+                    List.of(
+                            new Answer("answer11", true),
+                            new Answer("answer12"),
+                            new Answer("answer13")
+                    )
+            ),
+            new Question(2L, "question2",
+                    List.of(
+                            new Answer("answer21"),
+                            new Answer("answer22", true),
+                            new Answer("answer23")
+                    )
+            ),
+            new Question(3L, "question3",
+                    List.of(
+                            new Answer("answer31"),
+                            new Answer("answer32", true),
+                            new Answer("answer33")
+                    )
+            ),
+            new Question(4L, "question4",
+                    List.of(
+                            new Answer("answer41"),
+                            new Answer("answer42"),
+                            new Answer("answer43", true)
+                    )
+            ),
+            new Question(5L, "question5",
+                    List.of(
+                            new Answer("answer51"),
+                            new Answer("answer52"),
+                            new Answer("answer53", true)
+                    )
+            )
+    );
+
+    private QuestionDao questionDao;
+
+    private TestFileNameProvider testFileNameProvider;
+    private final String fileName = "test-questions.csv";
+
+    @BeforeEach
+    void setUp() {
+        testFileNameProvider = Mockito.mock(TestFileNameProvider.class);
+        BDDMockito.given(testFileNameProvider.getTestFileName()).willReturn(fileName);
+
+    }
+
+    @DisplayName("should be create correct List<Question> for given text")
+    @Test
+    void getAllTestWithMockConverter() {
+
+        questionDao = new CsvQuestionDao(testFileNameProvider);
+        List<Question> actualQuests = questionDao.findAll();
+        Assertions.assertIterableEquals(actualQuests, questionList);
+    }
+
+
+}

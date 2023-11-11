23630a79b01bbe8d546a01b7be6fecd5c0d577bd
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-11-11 16:20:34 +0400

    HomeWork4 refactoring in tests: CsvQuestionDaoTest & TestServiceImplTest

--- HW04/src/test/java/ru/otus/dao/CsvQuestionDaoTest.java
+++ HW04/src/test/java/ru/otus/dao/CsvQuestionDaoTest.java
@@ -5,12 +5,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.otus.config.TestFileNameProvider;
-import ru.otus.model.Answer;
+import ru.otus.fixtures.Quests;
 import ru.otus.model.Question;
 
 import java.util.List;
@@ -20,43 +19,7 @@ import java.util.List;
 class CsvQuestionDaoTest {
 
 
-    private final List<Question> questionList = List.of(
-            new Question(1L, "question1",
-                    List.of(
-                            new Answer("answer11", true),
-                            new Answer("answer12"),
-                            new Answer("answer13")
-                    )
-            ),
-            new Question(2L, "question2",
-                    List.of(
-                            new Answer("answer21"),
-                            new Answer("answer22", true),
-                            new Answer("answer23")
-                    )
-            ),
-            new Question(3L, "question3",
-                    List.of(
-                            new Answer("answer31"),
-                            new Answer("answer32", true),
-                            new Answer("answer33")
-                    )
-            ),
-            new Question(4L, "question4",
-                    List.of(
-                            new Answer("answer41"),
-                            new Answer("answer42"),
-                            new Answer("answer43", true)
-                    )
-            ),
-            new Question(5L, "question5",
-                    List.of(
-                            new Answer("answer51"),
-                            new Answer("answer52"),
-                            new Answer("answer53", true)
-                    )
-            )
-    );
+
 
     @Autowired
     private QuestionDao questionDao;
@@ -74,6 +37,8 @@ class CsvQuestionDaoTest {
     @DisplayName("should be create correct List<Question> for given text")
     @Test
     void getAllTestWithMockConverter() {
+        List<Question> questionList = Quests.getList();
+
         List<Question> actualQuests = questionDao.findAll();
 
         Assertions.assertIterableEquals(actualQuests, questionList);
--- HW04/src/test/java/ru/otus/service/TestServiceImplTest.java
+++ HW04/src/test/java/ru/otus/service/TestServiceImplTest.java
@@ -3,16 +3,13 @@ package ru.otus.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InOrder;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.util.ReflectionTestUtils;
 import ru.otus.dao.QuestionDao;
-import ru.otus.model.Answer;
+import ru.otus.fixtures.Quests;
 import ru.otus.model.Question;
 import ru.otus.model.Student;
 import ru.otus.model.TestResult;
@@ -49,7 +46,7 @@ class TestServiceImplTest {
     void shouldExecuteTestForStudent() {
         Student student = new Student("FirstNameOfStudent", "SecondName");
         TestResult expectedResult = new TestResult(student);
-        List <Question> questionList = generateQuestions();
+        List <Question> questionList = Quests.getList();
         ReflectionTestUtils.setField(expectedResult, "rightAnswersCount", 2);
         ReflectionTestUtils.setField(expectedResult, "answeredQuestions", questionList);
         when(questionDao.findAll()).thenReturn(questionList);
@@ -60,49 +57,10 @@ class TestServiceImplTest {
         TestResult actual = testService.executeTestFor(student);
 
         inOrder.verify(ioService, times(1)).printLine("");
-        inOrder.verify(ioService, times(1)).printLineLocalized(eq ("TestService.answer.the.questions"));
+        inOrder.verify(ioService, times(1)).printLineLocalized( ("TestService.answer.the.questions"));
         inOrder.verify(ioService, times(1)).printLine("");
         inOrder.verify(ioService, times(5)).printLine(anyString());
         assertThat(actual).usingRecursiveComparison().isEqualTo(expectedResult);
     }
 
-    private List<Question> generateQuestions() {
-        return List.of(
-                new Question(1L, "question1",
-                        List.of(
-                                new Answer("answer11", true),
-                                new Answer("answer12"),
-                                new Answer("answer13")
-                        )
-                ),
-                new Question(2L, "question2",
-                        List.of(
-                                new Answer("answer21"),
-                                new Answer("answer22", true),
-                                new Answer("answer23")
-                        )
-                ),
-                new Question(3L, "question3",
-                        List.of(
-                                new Answer("answer31"),
-                                new Answer("answer32", true),
-                                new Answer("answer33")
-                        )
-                ),
-                new Question(4L, "question4",
-                        List.of(
-                                new Answer("answer41"),
-                                new Answer("answer42"),
-                                new Answer("answer43", true)
-                        )
-                ),
-                new Question(5L, "question5",
-                        List.of(
-                                new Answer("answer51"),
-                                new Answer("answer52"),
-                                new Answer("answer53", true)
-                        )
-                )
-        );
-    }
 }

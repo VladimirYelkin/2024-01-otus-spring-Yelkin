661c39f79c25adc43b5187f19836d5a7ae066ee6
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-11-11 16:21:42 +0400

    HomeWork4 fix in LoggingAspectTest

--- HW04/src/test/java/ru/otus/logging/LoggingAspectTest.java
+++ HW04/src/test/java/ru/otus/logging/LoggingAspectTest.java
@@ -2,7 +2,6 @@ package ru.otus.logging;
 
 import org.aspectj.lang.JoinPoint;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,33 +11,18 @@ import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.slf4j.Logger;
 import ru.otus.fixtures.Quests;
-import ru.otus.model.Question;
 
-import java.lang.annotation.Annotation;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.InstanceOfAssertFactories.ARRAY;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.when;
-import static org.assertj.core.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class LoggingAspectTest {
 
     private LoggingAspect loggingAspect;
-    @Mock
-    private Logger logger;
 
     Object stubObjectClass;
     @Mock

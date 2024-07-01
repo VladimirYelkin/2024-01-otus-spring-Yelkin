499d052afe5481ce919ba13f7ec9dfa163021297
Vladimir Yelkin <vl.yelkin@gmail.com>
2024-07-01 23:03:43 +0400

    HW09 исправления по замечаниям

--- HW09/src/main/java/ru/otus/hw/services/CommentService.java
+++ HW09/src/main/java/ru/otus/hw/services/CommentService.java
@@ -3,7 +3,6 @@ package ru.otus.hw.services;
 import ru.otus.hw.dto.CommentDto;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CommentService {
     CommentDto findById(long id);
